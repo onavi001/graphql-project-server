bef527761c7da7a9dd479e9dc61ee0f4f570dacf
onavi001 <34868568+onavi001@users.noreply.github.com>
2024-06-27 22:34:47 -0600

    fix config

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,17 +1,32 @@
 const { ApolloServer, gql } = require('apollo-server-lambda');
+const mongoose = require('mongoose');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('../../schema/schema');
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hello, world!',
-  },
+const connectToDatabase = () => {
+  return mongoose.connect(`mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@cluster0.iiyve1i.mongodb.net/${process.env.mongoDatabase}?retryWrites=true&w=majority&appName=Cluster0`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  schema,
+  graphiql: true
+});
 
-exports.handler = server.createHandler();
+const handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  if (!mongoose.connection.readyState) {
+    await connectToDatabase();
+  }
+  const expressApp = express();
+  expressApp.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+  }));
+  return server.createHandler()(event, context);
+};
+
+exports.handler = handler;
